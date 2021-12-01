13893aace92d973945213807b934a49133c9447d
jhonboy121 <alfredmathew05@gmail.com>
2021-12-01 11:46:25 +0530

    overlays: bring back icon packs
    Partially reverts commit 126b2450b89ff9d9e9feee17a63fd7f2b1416a25.
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- icons/icons.mk
+++ icons/icons.mk
@@ -57,3 +57,36 @@ PRODUCT_PACKAGES += \
     InsideWiFiOverlay \
     BarsWiFiOverlay \
     DoraWiFiOverlay
+
+# Icon Packs
+PRODUCT_PACKAGES += \
+	IconPackCircularAndroidOverlay \
+	IconPackCircularLauncherOverlay \
+	IconPackCircularSettingsOverlay \
+	IconPackCircularSystemUIOverlay \
+	IconPackCircularThemePickerOverlay \
+	IconPackVictorAndroidOverlay \
+	IconPackVictorLauncherOverlay \
+	IconPackVictorSettingsOverlay \
+	IconPackVictorSystemUIOverlay \
+	IconPackVictorThemePickerOverlay \
+	IconPackSamAndroidOverlay \
+	IconPackSamLauncherOverlay \
+	IconPackSamSettingsOverlay \
+	IconPackSamSystemUIOverlay \
+	IconPackSamThemePickerOverlay \
+	IconPackKaiAndroidOverlay \
+	IconPackKaiLauncherOverlay \
+	IconPackKaiSettingsOverlay \
+	IconPackKaiSystemUIOverlay \
+	IconPackKaiThemePickerOverlay \
+	IconPackFilledAndroidOverlay \
+	IconPackFilledLauncherOverlay \
+	IconPackFilledSettingsOverlay \
+	IconPackFilledSystemUIOverlay \
+	IconPackFilledThemePickerOverlay \
+	IconPackRoundedAndroidOverlay \
+	IconPackRoundedLauncherOverlay \
+	IconPackRoundedSettingsOverlay \
+	IconPackRoundedSystemUIOverlay \
+	IconPackRoundedThemePickerOverlay \
