10dd9d0c07764099fa6c0937d955f9b0a4b4ec7b
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2022-02-09 06:56:58 -0500

    ColorBucket: Import themes from https://github.com/Project-Xtended/ve… …ndor_xtras
    Change-Id: Ib626392269ac7ce10dec19325939eb6a7ac51e8f

---
+++ themes/themes.mk
@@ -0,0 +1,16 @@
+# Themes
+PRODUCT_PACKAGES += \
+    XtendedClearSystemOverlay \
+    XtendedClearSystemUIOverlay \
+    PitchBlackSystemOverlay \
+    PitchBlackSystemUIOverlay \
+    SolarizedDarkSystemOverlay \
+    SolarizedDarkSystemUIOverlay \
+    BakedGreenSystemOverlay \
+    BakedGreenSystemUIOverlay \
+    ChocoXSystemOverlay \
+    ChocoXSystemUIOverlay \
+    MaterialOceanSystemOverlay \
+    MaterialOceanSystemUIOverlay \
+    DarkGreySystemOverlay \
+    DarkGreySystemUIOverlay
