87c3fdb4bb2e520dd42ec8b3f608407b003fb477
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2022-02-09 07:00:50 -0500

    overlays: Reenable themes
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>
    Change-Id: I8f68cc44ee826e2d22cdf693e39d0b375ac261c5

--- bliss_overlays.mk
+++ bliss_overlays.mk
@@ -61,7 +61,7 @@ endif
 #include vendor/overlays/accents/accents.mk
 
 # System themes
-#include vendor/overlays/themes/backgrounds/system_themes.mk
+include vendor/overlays/themes/themes.mk
 
 # Primary Colors
 #include vendor/overlays/primary/primary_colors.mk
